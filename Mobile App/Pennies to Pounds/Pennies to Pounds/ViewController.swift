6ee4b527e8e44cab96b64798b14dfd47bf4714e8
George McDonnell <g.a.mcdonnell7@gmail.com>
2016-03-11 21:04:44 +0000

    Initial mobile app project commit

---
+++ Mobile App/Pennies to Pounds/Pennies to Pounds/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pennies to Pounds
+//
+//  Created by George McDonnell on 11/03/2016.
+//  Copyright © 2016 George McDonnell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
